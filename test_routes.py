60b0839a257d36eaacdb870ba17479bc962a1960
Bradley Neve <brad.neve@gmail,com>
2022-10-31 20:15:13 +0000

    add some basic tests for routes

--- test_routes.py
+++ test_routes.py
@@ -10,12 +10,16 @@ class TestAPIcase():
     def test_allstats(self, api):
         resp = api.get('/allstats')
         assert resp.status == '200 OK'
-    # def test_stats/(self, api):
-    #     resp = api.get('/')
-    #     assert resp.status == '200 OK'
+    def test_stats_for_player(self, api):
+        resp = api.get('/stats/Granit Xhaka')
+        assert resp.status == '200 OK'
+    def test_team_stats(self, api):
+        resp = api.get('/teams/Manchester United F.C.')
+        assert resp.status == '200 OK'
+    def test_user_stats(self, api):
+        resp = api.get('/userplayer/5883591')
+        assert resp.status == '200 OK'
     # def test_home(self, api):
-    #     resp = api.get('/')
-    #     assert resp.status == '200 OK'
-    # def test_home(self, api):
-    #     resp = api.get('/')
-    #     assert resp.status == '200 OK'
+    #     data = {'email': 'brad.neve@gmail.com', 'password': 'Futureproof#1', 'userID': '5883591'}
+    #     resp = api.post('/getuserteam', data = data)
+    #     assert resp.status == '201 OK'
